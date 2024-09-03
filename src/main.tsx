b5239073d8335568f0c8a86aa873036afb1161fe
Aleksandr S. Nikitiuk <31177834+AleksandrNikitiuk@users.noreply.github.com>
2024-09-03 13:03:23 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import './index.css'
 import WebApp from '@twa-dev/sdk'
 import { MainContextProvider } from './Context/MainContext'
 
-WebApp.ready()
+WebApp.ready();
+WebApp.expand();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
