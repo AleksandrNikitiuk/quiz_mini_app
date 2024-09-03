fddbc1c2c80cd6f7cc70570ea6a1c1d5d2dff610
Aleksandr S. Nikitiuk <31177834+AleksandrNikitiuk@users.noreply.github.com>
2024-09-03 12:51:15 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,5 @@
 import QuizCompound from './Compounds/QuizCompound'
-import { useEffect, useRef } from 'react'
 
 export default function App() {
-  const appRef = useRef(null);
-
-  useEffect(() => {
-    const app = appRef.current;
-
-    const handleFullscreen = () => {
-      if (app.requestFullscreen) {
-        app.requestFullscreen();
-      } else if (app.mozRequestFullScreen) {
-        app.mozRequestFullScreen();
-      } else if (app.webkitRequestFullscreen) {
-        app.webkitRequestFullscreen();
-      }
-    };
-
-    // Проверяем, загрузилась ли страница полностью
-    window.addEventListener('load', handleFullscreen);
-
-    return () => {
-      window.removeEventListener('load', handleFullscreen);
-    };
-  }, []);
-  
-  return (
-    <div ref={appRef}>
-      <QuizCompound />
-    </div>
-  )
+  return <QuizCompound />
 }
