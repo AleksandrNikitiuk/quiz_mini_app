635907f1379e24a2ae7398413fa209a46bc062f9
Aleksandr S. Nikitiuk <31177834+AleksandrNikitiuk@users.noreply.github.com>
2024-09-05 09:26:19 +0500

    Update Illustration.tsx

--- src/Components/FinishPage/Illustration.tsx
+++ src/Components/FinishPage/Illustration.tsx
@@ -14,8 +14,8 @@ export default function Illustration() {
   return (
     <div className='finish-illustration'>
       {correctAnswersNumber + wrongAnswersNumber === correctAnswersNumber
-        ? <img src={WebApp.colorScheme === 'light'? stars: stars_dark } alt="Stars" />
-        : <img rel="preload" src="/accent.svg" /> /*<img src={WebApp.colorScheme === 'light'? accent: accent_dark } alt="Accent" />*/
+        ? <img rel="preload" src={WebApp.colorScheme === 'light'? stars: stars_dark } alt="Stars" />
+        : <img rel="preload" src={WebApp.colorScheme === 'light'? accent: accent_dark } alt="Accent" />
       }
     </div>
   )
